f288b0435183cd1556be1bf7adc204b8bb6210b4
kadirermaya <kadir.ermaya@gmail.com>
2020-06-09 11:09:00 -0700

    Completed Herd and Fleet classes with Robots and Dinosaurs

--- RobotVsDinosaurs/Fleet.cs
+++ RobotVsDinosaurs/Fleet.cs
@@ -7,11 +7,36 @@ namespace RobotVsDinosaurs
 
         // MEMBER VARIABLES ( HAS A )
         // I want the created Robot objects in Fleet
-        public List<Robot> robots = new List<Robot>();
+        public List<Robot> robots;
+        public List<Weapon> weapons;
+        public Fleet fleet;
+        public Robot robot1;
+        public Robot robot2;
+        public Robot robot3;
+        
+
         // CONSTRUCTER (SPAWNER)
         public Fleet()
         {
-            
+            robots = new List<Robot>();
+            weapons = new List<Weapon>();
+            fleet = new Fleet();
+            //move the instantiation of the three robots in here
+            //After instantiating, add them to the robots list
+
+            weapons.Add(new Weapon("sword", 3));
+            weapons.Add(new Weapon("Axe", 8));
+            weapons.Add(new Weapon("sword", 8));
+
+            robot1 = new Robot("Selçuk", weapons[1]);
+            robot2 = new Robot("Kadir", weapons[2]);
+            robot3 = new Robot("Mustafa", weapons[0]);
+
+            fleet.robots.Add(robot1);
+            fleet.robots.Add(robot2);
+            fleet.robots.Add(robot3);
+
+
         }
 
         //MEMBER METHODS
--- RobotVsDinosaurs/Herd.cs
+++ RobotVsDinosaurs/Herd.cs
@@ -8,18 +8,35 @@ namespace RobotVsDinosaurs
 {
     public class Herd
     {
-        
+
         // MEMBER VARIABLES ( HAS A )
 
-        public List<Dinosaur> dinosaurs = new List<Dinosaur>();
+        public List<Dinosaur> dinosaurs;
+        public Dinosaur dino1;
+        public Dinosaur dino2;
+        public Dinosaur dino3;
 
         // CONSTRUCTER (SPAWNER)
 
         public Herd()
         {
-            
+           dinosaurs = new List<Dinosaur>();
+
+            dino1 = new Dinosaur("Velociraptor", 3);
+            dino2 = new Dinosaur("Allosaurus", 5);
+            dino3 = new Dinosaur("Carnotaurus", 8);
+
+            dinosaurs.Add(dino1);
+            dinosaurs.Add(dino2);
+            dinosaurs.Add(dino3);
+
         }
 
         //MEMBER METHODS
+        public void AttackToRobot()
+        {
+
+        }
+
     }
 }
